abc703b80b0dba15e5b80e383b02508d4eaede49
Greg Blumberg <wblumberg@ou.edu>
2015-02-20 16:05:50 -0600

    Updated script to pull historical data to improve flexibility of script.

--- get_mesohistorical.py
+++ get_mesohistorical.py
@@ -48,22 +48,47 @@ def readData(url):
     data = np.genfromtxt( sound_data, dtype=str, skiprows=2)
     return data
 
-sndata = readData(get_url(datetime.strptime(yyyymmdd+hr, '%Y%m%d%H')))
+try:
+    sndata = readData(get_url(datetime.strptime(yyyymmdd+hr, '%Y%m%d%H')))
+except Exception,e:
+    print e
+    print "Program failed to read in data."
+    sys.exit()
 
+def win(wordlist, word_fragment):
+    boolean = []
+    for w in wordlist:
+        boolean.append(w.startswith(word_fragment))
+    return boolean
+
+# Open file for the data to be written to.
 n = open(yyyymmdd+hr+'.csv', 'w')
+pres = []
+for i in sndata[0][np.where(win(sndata[0], 'TAIR_'))[0]]:
+    pres.append(i.split('_')[1])
+
+tdry_idxs = np.where(win(sndata[0], 'TAIR_'))[0]
+tdew_idxs = np.where(win(sndata[0], 'TDEW_'))[0]
+hght_idxs = np.where(win(sndata[0], 'HGHT_'))[0]
+wspd_idxs = np.where(win(sndata[0], 'WDIR_'))[0]
+wdir_idxs = np.where(win(sndata[0], 'WSPD_'))[0]
 for i in range(1,len(sndata)):
-    station = sndata[i,0]
-    wmoid = sndata[i,1]
+    station = sndata[i,1]
+    wmoid = sndata[i,0]
     lat = sndata[i,3]
     lon = sndata[i,4]
     elev = sndata[i,5]
-    pres = sndata[i,7:17]# index 7 = 1000 mb pres 
-    temp = np.where(np.asarray(sndata[i,19:29], dtype=float) <= -991.0, '--', sndata[i,19:29])
-    dwpc = np.where(np.asarray(sndata[i,31:41], dtype=float) <= -991.0, '--', sndata[i,31:41])
-    wdir = np.where(np.asarray(sndata[i,43:53], dtype=float) <= -991.0, '--', sndata[i,43:53])
-    wspd = np.where(np.asarray(sndata[i,55:65], dtype=float) <= -991.0, '--', sndata[i,55:65])
-    hght = np.where(np.asarray(sndata[i,67:77], dtype=float) <= -991.0, '--', sndata[i,67:77])
+
+    temp = np.where(np.asarray(sndata[i,tdry_idxs], dtype=float) <= -991.0, '--', sndata[i,tdry_idxs])
+    dwpc = np.where(np.asarray(sndata[i,tdew_idxs], dtype=float) <= -991.0, '--', sndata[i,tdew_idxs])
+    wdir = np.where(np.asarray(sndata[i,wdir_idxs], dtype=float) <= -991.0, '--', sndata[i,wdir_idxs])
+    wspd = np.where(np.asarray(sndata[i,wspd_idxs], dtype=float) <= -991.0, '--', sndata[i,wspd_idxs])
+    hght = np.where(np.asarray(sndata[i,hght_idxs], dtype=float) <= -991.0, '--', sndata[i,hght_idxs])
+    if len(hght) < len(wspd):
+        hght = np.concatenate(([elev], hght))
     for j in range(len(hght)):
+        if pres[j] == 'SFC':
+            continue
         string = ','.join([wmoid, station, lat, lon, elev, pres[j], temp[j], dwpc[j], hght[j], wspd[j], wdir[j]])
         print string
         n.write(string + '\n')
